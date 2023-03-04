d30880864e568c5b0e1922b49e60bb882e100e71
Nico <nicodiazan@gmail.com>
2023-03-04 23:59:46 +0100

    Start work on the new parsing engine

---
+++ tokens.js
@@ -0,0 +1,50 @@
+
+//For context, here Negate == Opposite and Inverse == Reciprocal
+
+class TreeNode {
+
+}
+
+class Add extends TreeNode {
+
+}
+
+//To-Do: decide if Subtract(a,b) should be replaced with Add(a, new Negate(b))
+
+class Sub extends TreeNode {
+
+}
+
+class Mult extends TreeNode {
+
+}
+
+//To-Do: decide if Divide(a,b) should be replaced with Mult(a, new Inverse(b))
+
+class Div extends TreeNode {
+
+}
+
+//To-Do: Actual implementation of the modulo operator
+
+class Mod extends TreeNode {
+
+}
+
+class Negate extends TreeNode {
+
+}
+
+//To-Do: Actual implementation of the inverse operator
+
+class Inverse extends TreeNode {
+
+}
+
+class ID extends TreeNode {
+
+}
+
+class Int extends TreeNode {
+
+}
